afa927e3739a14b869f01d84dcbe93f95898d7a0
Benjamin Wang <benjylxwang64@gmail.com>
2021-11-11 20:13:28 +0000

    Created first build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import logo512 from "./assets/logo/logo512.png";
